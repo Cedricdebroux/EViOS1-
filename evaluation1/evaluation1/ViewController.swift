b8d63a9da72dc6f1f3f55f22393ef08de2705ce2
Cedricdebroux <cedric@debroux.ovh>
2022-07-07 12:34:48 +0200

    evaluation1-exo3-complete

--- evaluation1/evaluation1/ViewController.swift
+++ evaluation1/evaluation1/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     @IBOutlet var userImage : UIImageView!
     @IBOutlet var passwordField : UITextField!
     @IBOutlet var passwordImage : UIImageView!
+    @IBOutlet var login : UITextField!
+    @IBOutlet var switchButton : UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,10 +41,45 @@ class ViewController: UIViewController {
             passwordField.isSecureTextEntry = true
             passwordImage.image = UIImage(named: "eye_on_icon")
         }
-        print("test button")
-        
+    }
+    
+    @IBAction func saveContact(){
+        print("Le login est : \(login.text ?? "")")
+        print("Le mot de passe : \(passwordField.text ?? "")")
+        print("La newletter est coché \(switchButton.isOn)")
+    }
+    
+    func showAlert() {
+        if switchButton.isOn == true{
+            let alert = UIAlertController(title: "Bienvenue \(login.text ?? "")", message: "Vous vous êtes inscrit à la newletter", preferredStyle: .alert)
+               alert.addAction(UIAlertAction(title: "Merci !", style: .default, handler: nil))
+               present(alert, animated: true)
+        }
+        else {
+            let alert = UIAlertController(title: "Bienvenue \(login.text ?? "")", message: "Vous  n'êtes pas inscrit à la newletter", preferredStyle: .alert)
+               alert.addAction(UIAlertAction(title: "Merci !", style: .default, handler: nil))
+               present(alert, animated: true)
+        }
+       }
+    func showError(){
+        let alert = UIAlertController(title: "ERROR", message: "Une condition n'est pas respecté", preferredStyle: .alert)
+           alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+           present(alert, animated: true)
+    }
+    
+    @IBAction func show(){
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+       
+        let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        if predicate.evaluate(with: login.text) && passwordField.text?.count ?? 0 >= 4 {
+            showAlert()
+        } else{
+            showError()
+        }
+            
     }
 }
+
 
 extension UIImageView {
     
